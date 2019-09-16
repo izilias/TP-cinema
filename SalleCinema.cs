1f99a41b2e39fa74d8114e0be4fe090f58edfa55
isalah <isalah@S-204-04>
2019-09-16 09:39:40 +0200

    Première initialisation

---
+++ SalleCinema.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpCinema
+{
+    class SalleCinema
+    {
+        private string titre;
+        private int nbplaces;
+        private double tarif;
+        private int nbplacesvn;
+        private int nbplacesvr;
+
+        public SalleCinema(string leTitre, int nombrePlaces, double leTarif)
+        {
+            this.titre = leTitre;
+            this.nbplaces = nombrePlaces;
+            this.tarif = leTarif;
+            nbplacesvn = 0;
+            nbplacesvr = 0;
+        }
+
+        public int nbPlacesDisponibles()
+        {
+            return nbplaces;
+        }
+    }
+}
